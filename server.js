773a074d88851bd39da17854608b223808193aac
KateMatchica <matchicakate@gmail.com>
2026-01-02 16:24:06 +0800

    Add files via upload
    Initial deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+const PORT = 3000;
+app.listen(PORT, () = {
+    console.log(`Digital Herbarium running at httplocalhost${PORT}`);
+});
